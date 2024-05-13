c16edbc69fb49d83a09016d5bfd3dc397ec5c23d
frcan89 <frcan89@gmail.com>
2024-05-12 20:48:28 -0500

    Ejercicio 01 operadores

--- Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/frcan89.js
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/javascript/frcan89.js
@@ -1,6 +1,40 @@
-//Assignment operators
+//Operadores Aritméticos
 
-let x = 3;
-console.log(x);
-x += 1;
-console.log(x);
+let suma = 1 + 1; // 2
+let resta = 3 -1; // 2
+let multiplicacion = 3 * 2 // 6
+let division = 6 / 3; // 2
+let modulo = 5 % 3; // 2
+let incremento = 0;
+    incremento++ //Primto se ejecuta luego incremente
+    ++incremento // Primero incremente depues ejecuta
+let decremento = 5; 
+    decremento-- //Primto se ejecuta luego decrementa
+    --decremento // Primero decrementa depues ejecuta  
+
+//Operadores de asignación 
+
+let x = 11; //Asignación
+x += 1; //Asignación Addition
+x -= 1; //Asignación Subtraction
+x *= 2; //Asignación Multiplicación
+x /= 2; //Asignación División
+x %= 2; // Asignación Residuo
+x **= 2; //Asignación Exponenciación
+x <<= 2; //Asignación de desplazamiento a la izquierda
+x >>= 2; // Asignación de desplazamiento a la Derecha
+x &= 2; //Asignación bit a bit AND
+x |= 2; //Asignación bit a bit OR
+x ^= 2; //Asignación bit a bit XOR
+
+// Operadores de Comparación
+3 == "3"; //Es igual
+5 != 3; //No es igual
+3 === "3"; // Igualdad Estricta
+5 !== "3"; // Desigualdad Estricta
+5 > 3; //Mayor que 
+5 >= 5; //Mayor o Igual
+3 < 5; //Menor que
+3 <= 5; //Menor o Igial
+
+//
